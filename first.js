32e92128a6ff979a7547a4da079215c615d8d5a9
simran sahani <simransahani936@gmail.com>
2024-08-02 12:13:12 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,21 @@
+let score= "98abc" //if we put null it shows 0,and on defined it shows NaN,True =1,false=0.
+console.log(typeof(score));//string 
+
+
+let valueInNumber=Number(score)// it is converted into number
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);// NaN
+
+
+let isloggedin=1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(typeof booleanisloggedin);
+console.log(booleanisloggedin);//true
+//1 --> true
+//0--> false
+"simran" -->true
+""   --> false
+
+let somenumber= 99
+let stringinnumber=String(somenumber)
+console.log( typeof stringinnumber);
